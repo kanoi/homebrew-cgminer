ea7a86a1a5191ccd980926d5c2e65f5e2a14317c
kanoi <gitkanoi@k1k2.com>
2021-11-07 17:34:40 +1100

    brew rb

---
+++ cgminer.rb
@@ -0,0 +1,28 @@
+class Cgminer < Formula
+  desc "cgminer"
+  homepage "https://kano.is/"
+  url "https://github.com/kanoi/cgminer.git", :branch => "master"
+  version "4.12.0"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "coreutils" => :build
+  depends_on "curl"
+  depends_on "libusb"
+
+  def install
+    system "autoreconf -fi"
+    system "./autogen.sh", "--disable-debug",
+                           "--disable-dependency-tracking",
+                           "--prefix=#{prefix}",
+                           "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig",
+                           "--enable-gekko"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
