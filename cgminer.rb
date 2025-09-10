f167caa40b4947b66f4f23d0b4a98ce58e4312f2
kanoi <gitkanoi@k1k2.com>
2025-09-10 12:58:34 +1000

    name 4.13.4

--- cgminer.rb
+++ cgminer.rb
@@ -2,7 +2,7 @@ class Cgminer < Formula
   desc "cgminer"
   homepage "https://kano.is/"
   url "https://github.com/kanoi/cgminer.git", :branch => "master"
-  version "4.13.3"
+  version "4.13.4"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
